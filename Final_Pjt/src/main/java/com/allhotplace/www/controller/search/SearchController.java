3726caecf562b9424e1e9928d102c263cbe8f609
sjkim1014 <47809977+sjkim1014@users.noreply.github.com>
2019-05-10 01:23:15 +0900

    커밋 테스트

--- Final_Pjt/src/main/java/com/allhotplace/www/controller/search/SearchController.java
+++ Final_Pjt/src/main/java/com/allhotplace/www/controller/search/SearchController.java
@@ -224,7 +224,7 @@ public class SearchController {
                result2 = result2 + line;
             }
             
-            JSONParser jsonParser = new JSONParser();
+             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObj = (JSONObject)jsonParser.parse(result2);
             
          } catch (Exception e) {
