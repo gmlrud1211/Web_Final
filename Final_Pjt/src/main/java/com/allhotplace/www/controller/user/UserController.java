d2fd55f79aa959c5bf58c063ff25fa0d6cb2a433
heekyoung <gmlrud1211@naver.com>
2019-05-10 03:15:18 +0900

    클래스파일 삭제, gitignore 수정

--- Final_Pjt/src/main/java/com/allhotplace/www/controller/user/UserController.java
+++ Final_Pjt/src/main/java/com/allhotplace/www/controller/user/UserController.java
@@ -73,9 +73,11 @@ public class UserController {
 		  System.out.println(session);
 		  session.setAttribute("login", true);
 		  
+		  
 		  System.out.println(vo.toString());
 		  session.setAttribute("user_id", vo.getUser_name());
 			
+		  
 		return "/user/kakaoLogin";
 	}
 
