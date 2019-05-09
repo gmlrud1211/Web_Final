8cfc7e6a944125b21827792754189503cf7462f1
sjkim1014 <47809977+sjkim1014@users.noreply.github.com>
2019-05-10 01:26:11 +0900

    커밋테스트

--- Final_Pjt/src/main/java/com/allhotplace/www/controller/search/SearchController.java
+++ Final_Pjt/src/main/java/com/allhotplace/www/controller/search/SearchController.java
@@ -222,7 +222,7 @@ public class SearchController {
             String line;
             while ((line = br2.readLine()) != null) {
                result2 = result2 + line;
-            }
+             }
             
              JSONParser jsonParser = new JSONParser();
             JSONObject jsonObj = (JSONObject)jsonParser.parse(result2);
